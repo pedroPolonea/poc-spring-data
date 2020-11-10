5950d937ce8f6617f70ccef6dd9e83c9e44bf69c
pedroPolonea <phpolonea@hotmail.com>
2020-11-10 00:56:00 -0300

    Add script init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into products(ID_PRODUCT, NAME, DESCRIPTION, AMOUNT, UNITARY_VALUE, ACQUISITION_VALUE, DAT_ACQUISITION, DAT_CREATION, ACTIVE, PRODUCT_TYPE)
+values(1, 'TV', 'TV LG', 10, 1000, 500, now(), now(), 1, 'ELECTRONICS'),
+      (2, 'Sabonete', 'Morango', 10, 5, 1, now(), now(), 1, 'BATHROOM'),
+      (3, 'Panela', 'Aluminio', 4, 29, 5, now(), now(), 1, 'KITCHEN'),
+      (4, 'Radio', 'LG', 30, 50, 10, now(), now(), 1, 'ELECTRONICS');
